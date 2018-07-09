9828c5ad41dbec343da58474072e93f0c1eafea2
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-07-09 13:10:37 -0400

    passing 1 plus 1 is 2

--- calculator_test.rb
+++ calculator_test.rb
@@ -3,6 +3,6 @@ require_relative "tdd_calculator.rb"
 class CalculatorTest < Minitest::Test
 
    def test_assert_that_1_plus_1_equals_2
-	assert_equal(2, addition(3,1))
+	assert_equal(2, addition(1,1))
    end
 end
