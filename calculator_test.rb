8fbaec0a43952554e6ef83474ea621e603fff9f8
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-07-09 14:04:59 -0400

    complete the calculator app w/n error

--- calculator_test.rb
+++ calculator_test.rb
@@ -5,4 +5,20 @@ class CalculatorTest < Minitest::Test
    def test_assert_that_1_plus_1_equals_2
 	assert_equal(2, addition(1,1))
    end
+  
+   def test_assert_subtract_answer
+   	assert_equal(1, subtract(3,2))
+   end
+
+   def test_assert_multiply_answer
+   	assert_equal(56, multiply(8,7))
+   end
+   def test_assert_divide_answer
+   	assert_equal(4, divide(8,2))
+   end
+   #def test_assert_divide_by_zero
+   	#assert_equal(0, divide(90, 0))
+   	def test_if_division_by_zero_gets_nil
+   		assert_equal(nil, divide(8, 0))
+   	end
 end
--- tdd_calculator.rb
+++ tdd_calculator.rb
@@ -1,3 +1,17 @@
 def addition (first_number, second_number)
 	first_number + second_number
 end
+def subtract(first, second)
+	first - second
+end
+def multiply(first, second)
+	anwer = first * second
+end
+def divide(first, second)
+	if second == 0 
+		nil
+	else 
+		answer = first / second
+	end
+	answer
+end
